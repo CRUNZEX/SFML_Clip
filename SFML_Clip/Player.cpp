6a1fa1570884d30112932538c5da313a7c21e8a5
CRUNZEX <crunzex@gmail.com>
2020-11-08 17:45:27 +0700

    Add project files.

---
+++ SFML_Clip/Player.cpp
@@ -0,0 +1,77 @@
+#include "Player.h"
+
+void Player::initVariables()
+{
+}
+
+void Player::initTexture()
+{
+	if (!this->texture.loadFromFile("Pictures/Player.png"))
+		printf("ERROR::Texture.Player >> Not Load");
+}
+
+void Player::initSprite()
+{
+	this->sprite.setTexture(this->texture);
+	this->sprite.setScale(1.f, 1.f);
+
+	this->currentFrame = sf::IntRect(0, 0, 100, 100);
+
+	this->sprite.setPosition(300, 300);
+
+	this->sprite.setTextureRect(this->currentFrame);
+}
+
+Player::Player()
+{
+	this->initVariables();
+	this->initTexture();
+	this->initSprite();
+
+	this->movementSpeed = 5.f;
+}
+
+Player::~Player()
+{
+}
+
+void Player::setPosition(const float x, const float y)
+{
+	this->sprite.setPosition(x, y);
+}
+
+void Player::move(const float dirX, const float dirY)
+{
+	this->sprite.move(this->movementSpeed * dirX, this->movementSpeed * dirY);
+}
+
+void Player::update()
+{
+	this->updateMovement();
+}
+
+void Player::render(sf::RenderTarget& target)
+{
+	target.draw(this->sprite);
+}
+
+void Player::updateMovement()
+{
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+	{
+		this->move(-1.f, 0.f);
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	{
+		this->move(1.f, 0.f);
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+	{
+		this->move(0.f, -1.f);
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+	{
+		this->move(0.f, 1.f);
+	}
+}
---
+++ SFML_Clip/Player.h
@@ -0,0 +1,37 @@
+#pragma once
+#include<SFML/Graphics.hpp>
+#include<SFML/System.hpp>
+#include<stdio.h>
+
+class Player
+{
+private:
+	sf::Sprite sprite;
+	sf::Texture texture;
+
+	sf::IntRect currentFrame;
+
+	//Core
+	void initVariables();
+	void initTexture();
+	void initSprite();
+
+	float movementSpeed;
+	
+	sf::RectangleShape body;
+
+public:
+	Player();
+	virtual ~Player();
+
+	//modifier
+	void setPosition(const float x, const float y);
+	void move(const float dirX, const float dirY);
+
+	//functions
+	void update();
+	void render(sf::RenderTarget& target);
+
+	void updateMovement();
+};
+
